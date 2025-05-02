0197c4dae06d47e72a9fd8d6a1b94f20b9814717
CrispyDanger <crispyd2405@gmail.com>
2025-05-02 17:37:15 +0200

    adjust allowed host

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-*t^o_rm^x#w-zhq(ojzcq)d9$q#&1)p5jhwuqz22sr&%txd#d*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
