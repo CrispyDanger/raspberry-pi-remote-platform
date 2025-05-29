87cd81d9439560316cdff1f4434eebee909201d3
CrispyDanger <crispyd2405@gmail.com>
2025-05-29 15:19:48 +0200

    update utils.py and remove init_gpio

--- control/utils.py
+++ control/utils.py
@@ -1,9 +1,33 @@
-from .gpio_setup import left_pwm, right_pwm, init_gpio
+import RPi.GPIO as GPIO
 
 const_frequency = 50
 const_gpio_left_pin = 18
 const_gpio_right_pin = 12
 const_duty_cycle_discrete = 1
+
+# Globals (per-process safe)
+IS_INITIALIZED = False
+left_pwm = None
+right_pwm = None
+
+
+def init_gpio():
+    global IS_INITIALIZED, left_pwm, right_pwm
+
+    if IS_INITIALIZED:
+        return
+
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(const_gpio_left_pin, GPIO.OUT)
+    GPIO.setup(const_gpio_right_pin, GPIO.OUT)
+
+    left_pwm = GPIO.PWM(const_gpio_left_pin, const_frequency)
+    right_pwm = GPIO.PWM(const_gpio_right_pin, const_frequency)
+    left_pwm.start(0)
+    right_pwm.start(0)
+
+    IS_INITIALIZED = True
 
 
 def pwm_control(pwm_obj, duty_cycle):
@@ -11,7 +35,9 @@ def pwm_control(pwm_obj, duty_cycle):
 
 
 def write_operation(operation, speed=1):
+    global left_pwm, right_pwm
     init_gpio()
+
     dc = speed + const_duty_cycle_discrete
 
     match operation:
