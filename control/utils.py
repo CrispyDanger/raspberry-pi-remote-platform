8afde72479cea4612b3f4249eec9f46c43aa802c
CrispyDanger <crispyd2405@gmail.com>
2025-05-29 14:20:20 +0200

    rewrite utils.py for new library Rpi.GPIO

--- control/utils.py
+++ control/utils.py
@@ -1,40 +1,50 @@
-from gpiozero import Motor
-from time import sleep
-from gpiozero.pins.pigpio import PiGPIOFactory
+import RPi.GPIO as GPIO
 
-factory = PiGPIOFactory()
-left_motor = Motor(forward=12, backward=18, pwm=True, pin_factory=factory)
-right_motor = Motor(forward=13, backward=19, pwm=True, pin_factory=factory)
+const_frequency = 50
+const_gpio_left_pin = 18
+const_gpio_right_pin = 12
+const_duty_cycle_discrete = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(const_gpio_left_pin, GPIO.OUT)
+GPIO.setup(const_gpio_right_pin, GPIO.OUT)
+
+left_pwm = GPIO.PWM(const_gpio_left_pin, const_frequency)
+right_pwm = GPIO.PWM(const_gpio_right_pin, const_frequency)
+left_pwm.start(0)
+right_pwm.start(0)
 
 
-def write_operation(operation, speed=0):
+def pwm_control(pwm_obj, duty_cycle):
+    pwm_obj.ChangeDutyCycle(duty_cycle)
+
+
+def write_operation(operation, speed=1):
+    dc = speed + const_duty_cycle_discrete
 
     match operation:
         case 'forward':
-            left_motor.forward(speed)
             print('GOING FORWARD')
-            right_motor.forward(speed)
-            sleep(2)
+            pwm_control(left_pwm, dc)
+            pwm_control(right_pwm, dc)
 
         case 'backward':
-            left_motor.backward(speed)
-            print('GOING BACKWARDS')
-            right_motor.backward(speed)
-            sleep(2)
+            print('GOING BACKWARD')
+            # Inversion depends on your motor driver wiring
+            pwm_control(left_pwm, 0)
+            pwm_control(right_pwm, 0)
 
         case 'left':
-            left_motor.backward(speed)
-            print('GOING LEFT')
-            right_motor.forward(speed)
-            sleep(1)
+            print('TURNING LEFT')
+            pwm_control(left_pwm, 0)
+            pwm_control(right_pwm, dc)
 
         case 'right':
-            left_motor.forward(speed)
-            print('GOING RIGHT')
-            right_motor.backward(speed)
-            sleep(1)
+            print('TURNING RIGHT')
+            pwm_control(left_pwm, dc)
+            pwm_control(right_pwm, 0)
 
         case 'stop':
-            print('STOP')
-            left_motor.stop()
-            right_motor.stop()
+            print('STOPPING')
+            pwm_control(left_pwm, 0)
+            pwm_control(right_pwm, 0)
